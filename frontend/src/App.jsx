a8c2d56418b86f189529a9c6d16dad3d0758e063
MohammedYaseenkalvium <m.yaseensayed09@gmail.com>
2025-12-22 21:39:57 +0400

    frontend setup is done initially

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+const App = () => {
+  return (
+    <div className="text-4xl text-rose-500 font-bold">
+      Frontend is back 🚀
+    </div>
+  )
+}
+
+
+
+export default App
